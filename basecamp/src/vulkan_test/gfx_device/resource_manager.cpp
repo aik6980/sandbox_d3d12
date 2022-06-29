8df0f2b07df0ad05ac7f209cd3fc3833283b57b2
aik6980 <aik6980>
2022-06-29 23:27:21 +0100

    missing file

--- basecamp/src/vulkan_test/gfx_device/resource_manager.cpp
+++ basecamp/src/vulkan_test/gfx_device/resource_manager.cpp
@@ -8,6 +8,6 @@ void Resource_manager::create_vertex_buffer(uint32_t byte_size)
 {
     auto&& createinfo =
         vk::BufferCreateInfo({}, byte_size, vk::BufferUsageFlagBits::eVertexBuffer | vk::BufferUsageFlagBits::eTransferDst, vk::SharingMode::eExclusive);
-    auto&& allocinfo = vma::AllocationCreateInfo();
+    auto&& allocinfo = vma::AllocationCreateInfo({});
 }
 } // namespace VKN
